24057151d6d538d6fc9415cd84d4ac5a2aa260fd
Fatiha Ait kbir <140711537+Fatiha09@users.noreply.github.com>
2024-09-18 13:13:05 +0200

    Update Agent_REST.py

--- REST_Methods/Agent_REST.py
+++ REST_Methods/Agent_REST.py
@@ -25,7 +25,8 @@ import time
 
 
 # Set the environment variables from shell environment
-#-----------------------------------------------------
+#------------------Codestral-----------------------------------
+
 OPENAI_API_KEY = "YOUR_API_KEY"
 OPENAI_CHAT_MODEL = "solidrust/Codestral-22B-v0.1-hf-AWQ"
 OPENAI_CHAT_API_URL = "https://isdm-chat.crocc.meso.umontpellier.fr/openai"
@@ -35,7 +36,7 @@ llm = ChatOpenAI(
     openai_api_base=OPENAI_CHAT_API_URL,
 )
 
-#-------------------------------------
+#---------------------mixtral----------------
 
 from langchain_community.chat_models import ChatOllama
 
@@ -46,6 +47,11 @@ LLM_API_URL = "https://chat.crocc.meso.umontpellier.fr/ollama"
 llm = ChatOllama(model=LLM_MODEL, base_url=LLM_API_URL,
 headers={"Authorization": "Bearer " + LLM_JWT_BEARER,"Content-Type":"application/json",})
 
+#--------------------------llama3:8b--ollama--------------------
+
+# from langchain_community.llms import Ollama
+# MODEL = "llama3:8b"
+# llm = Ollama(model=MODEL)
 
 # Define variables used for processing
 #-------------------------------------
