0f05d3038294d7456e8454e7b8c105afdccb31c9
Fatiha Ait kbir <140711537+Fatiha09@users.noreply.github.com>
2024-09-19 16:43:47 +0200

    Update Agent_REST.py

--- REST_Methods_ollama/Agent_REST.py
+++ REST_Methods_ollama/Agent_REST.py
@@ -3,12 +3,6 @@ from langchain.prompts import PromptTemplate
 from langchain_openai import ChatOpenAI
 import requests
 import urllib.parse
-
-from langchain_core.prompts.chat import (
-    ChatPromptTemplate,
-    HumanMessagePromptTemplate,
-    SystemMessagePromptTemplate,
-)
 import pandas as pd
 import re
 import json
@@ -22,36 +16,11 @@ import faiss
 import numpy as np
 import time
 
-
-
-# Set the environment variables from shell environment
-#------------------Codestral-----------------------------------
-
-OPENAI_API_KEY = "YOUR_API_KEY"
-OPENAI_CHAT_MODEL = "solidrust/Codestral-22B-v0.1-hf-AWQ"
-OPENAI_CHAT_API_URL = "https://isdm-chat.crocc.meso.umontpellier.fr/openai"
-llm = ChatOpenAI(
-    model=OPENAI_CHAT_MODEL,
-    openai_api_key=OPENAI_API_KEY,
-    openai_api_base=OPENAI_CHAT_API_URL,
-)
-
-#---------------------mixtral----------------
-
-from langchain_community.chat_models import ChatOllama
-
-LLM_MODEL = "mixtral:8x7b-instruct-v0.1-q5_0"
-LLM_JWT_BEARER = "YOUR_API_KEY"
-LLM_API_URL = "https://chat.crocc.meso.umontpellier.fr/ollama"
-
-llm = ChatOllama(model=LLM_MODEL, base_url=LLM_API_URL,
-headers={"Authorization": "Bearer " + LLM_JWT_BEARER,"Content-Type":"application/json",})
-
 #--------------------------llama3:8b--ollama--------------------
 
-# from langchain_community.llms import Ollama
-# MODEL = "llama3:8b"
-# llm = Ollama(model=MODEL)
+from langchain_community.llms import Ollama
+MODEL = "llama3:8b"
+llm = Ollama(model=MODEL)
 
 # Define variables used for processing
 #-------------------------------------
