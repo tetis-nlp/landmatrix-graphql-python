800298ffda915ecfb5b82702db164f8f4864c093
Fatiha Ait kbir <140711537+Fatiha09@users.noreply.github.com>
2024-09-20 17:19:52 +0200

    Update RAG_GraphQL.py

--- GraphQL_Methods_Ollama/RAG_GraphQL.py
+++ GraphQL_Methods_Ollama/RAG_GraphQL.py
@@ -19,40 +19,19 @@ import faiss
 import numpy as np
 import time
 
-
-
-# Set the environment variables from shell environment
-#------------------Codestral-----------------------------------
-
-OPENAI_API_KEY = "YOUR_API_KEY"
-OPENAI_CHAT_MODEL = "solidrust/Codestral-22B-v0.1-hf-AWQ"
-OPENAI_CHAT_API_URL = "https://isdm-chat.crocc.meso.umontpellier.fr/openai"
-llm = ChatOpenAI(
-    model=OPENAI_CHAT_MODEL,
-    openai_api_key=OPENAI_API_KEY,
-    openai_api_base=OPENAI_CHAT_API_URL,
-)
-
-#---------------------mixtral----------------
-
-# from langchain_community.chat_models import ChatOllama
-
-# LLM_MODEL = "mixtral:8x7b-instruct-v0.1-q5_0"
-# LLM_JWT_BEARER = "YOUR_API_KEY"
-# LLM_API_URL = "https://chat.crocc.meso.umontpellier.fr/ollama"
-
-# llm = ChatOllama(model=LLM_MODEL, base_url=LLM_API_URL,
-# headers={"Authorization": "Bearer " + LLM_JWT_BEARER,"Content-Type":"application/json",})
-
 #--------------------------llama3:8b--ollama--------------------
 
-# from langchain_community.llms import Ollama
-# MODEL = "llama3:8b"
-# llm = Ollama(model=MODEL)
+from langchain_community.llms import Ollama
+MODEL = "llama3:8b"
+llm = Ollama(model=MODEL)
 
 # Define variables used for processing
 #-------------------------------------
 context = Context_GraphQL.context_GraphQL_RAG
+
+file = "Queries_GraphQL.xlsx"
+db = pd.read_excel(file)
+parser = StrOutputParser()
 
 # RAG: Retrieve similar questions based on the user's question
 #-------------------------------------------------------------
@@ -165,11 +144,7 @@ def ChatBot_response( llm, parser, context, question):
 
 
 # Test the model
-#--------------------------------------------------------
-
-file = "Queries_GraphQL.xlsx"
-db = pd.read_excel(file)
-parser = StrOutputParser()
+#-------------------------------------------------------
 
 def response(data, llm, parser, context):
     data['response'] = ''
