e3987069cd021d43caf02abdc05fdcb2bba57ef2
Decoupes Remy <remy.decoupes@gmail.com>
2024-09-22 22:52:42 +0200

    pipeline: fix arg error on model name

--- src/experiments.py
+++ src/experiments.py
@@ -10,14 +10,18 @@ ISDM_API_KEY = credential_config['ISDM']["ISDM_API_KEY"]
 ISDM_JWT = credential_config['ISDM']["ISDM_JWT"]
 list_of_models = ast.literal_eval(credential_config['ISDM']["MODEL_LIST"])
 
+list_of_models = ["solidrust/Codestral-22B-v0.1-hf-AWQ", 
+                "mixtral:8x7b-instruct-v0.1-q5_0", 
+                "llama3:8b"]
+
 list_of_expe = ["agent_graphql", "rag_graphql", "in_context_learning_graphql"]
 
 for expe in list_of_expe:
     print(expe)
     for model in list_of_models:
-        print(model)
+        print(f"\t|--> {model}")
         try:
             # Start the script with the given model in a new process
-            process = subprocess.Popen(['python', f'src/{expe}.py', '--api_key', ISDM_API_KEY, '--api_jwt',  ISDM_JWT, '--model', model])
+            process = subprocess.Popen(['python', f'src/{expe}.py', '--api_key', ISDM_API_KEY, '--api_jwt',  ISDM_JWT, '--model', f'"{model}"'])
         except Exception as e:
             print(f"Failed to start script with model {model}: {e}")
