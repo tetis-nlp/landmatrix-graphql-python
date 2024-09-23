cd4e411648bdb489a02b8e755dc601526c96378d
Fatiha Ait kbir <140711537+Fatiha09@users.noreply.github.com>
2024-09-23 14:02:46 +0200

    add methods to experiments

--- src/experiments.py
+++ src/experiments.py
@@ -14,7 +14,7 @@ list_of_models = ["solidrust/Codestral-22B-v0.1-hf-AWQ",
                 "mixtral:8x7b-instruct-v0.1-q5_0", 
                 "llama3:8b"]
 
-list_of_expe = ["agent_graphql", "rag_graphql", "in_context_learning_graphql"]
+list_of_expe = ["agent_graphql", "rag_graphql", "in_context_learning_graphql","agent_rest","rag_rest","in_context_learning_rest"]
 
 for expe in list_of_expe:
     print(expe)
