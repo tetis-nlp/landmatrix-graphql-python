264a470c576efe941a26164f934e375a1e705544
remydecoupes <remy.decoupes@gmail.com>
2024-10-07 18:02:34 +0200

    run sequentially all the experiments

--- src/experiments.py
+++ src/experiments.py
@@ -14,7 +14,7 @@ list_of_models = ["solidrust/Codestral-22B-v0.1-hf-AWQ",
                 "mixtral:8x7b-instruct-v0.1-q5_0", 
                 "llama3:8b"]
 
-list_of_expe = ["agent_graphql", "rag_graphql", "in_context_learning_graphql","agent_rest","rag_rest","in_context_learning_rest"]
+list_of_expe = ["agent_rest","rag_rest","in_context_learning_rest", "agent_graphql", "rag_graphql", "in_context_learning_graphql"]
 
 for expe in list_of_expe:
     print(expe)
@@ -23,5 +23,6 @@ for expe in list_of_expe:
         try:
             # Start the script with the given model in a new process
             process = subprocess.Popen(['python', f'src/{expe}.py', '--api_key', ISDM_API_KEY, '--api_jwt',  ISDM_JWT, '--model', f'{model}'])
+            process.wait()
         except Exception as e:
             print(f"Failed to start script with model {model}: {e}")
